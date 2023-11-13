5e32ffc7da09c7b6b0c4776bd53627a6f19659aa
pedrodoy <pedrodoy01@gmail.com>
2023-11-13 20:52:14 -0300

    page_videos

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:fitness/view/main_tab/aba_principal.dart';
 import 'package:fitness/view/main_tab/videos.dart';
 import 'package:fitness/view/login/cadastro.dart';
 import 'package:fitness/view/on_boarding/primeira_pagina.dart';
+import 'package:fitness/view/videos/supino_reto.dart';
 import 'package:flutter/material.dart';
 
 import 'common/cores.dart';
@@ -23,8 +24,8 @@ class MyApp extends StatelessWidget {
         primaryColor: TColor.primaryColor1,
         fontFamily: "Poppins"
       ),
-      home: const MainTabView(),
+      // ***home: const MainTabView(),
+      home: VideoPlayer(),
     );
   }
 }
-
