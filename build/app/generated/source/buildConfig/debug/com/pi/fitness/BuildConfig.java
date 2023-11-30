18b59f555078af10a2ac2fe6dec326f9cdaf0a9d
CaduZ28 <cadudcv@gmail.com>
2023-11-30 16:28:31 -0300

    update

--- build/app/generated/source/buildConfig/debug/com/pi/fitness/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/pi/fitness/BuildConfig.java
@@ -5,7 +5,7 @@ package com.pi.fitness;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.codeforany.fitness";
+  public static final String APPLICATION_ID = "com.pi.fitness";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
